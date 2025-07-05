2abdc6bbf452ca395f1d295698ede126bd1d7557
GabrielAlves263 <jgabrielalves@alu.ufc.br>
2025-07-05 20:47:35 -0300

    fix: correct greeting punctuation and adjust gap in main layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,9 +7,9 @@ export default function Home() {
   return (
     <div className="min-h-screen flex flex-col items-center bg-[#FAF9F6]">
       <Header />
-      <main className="w-full flex bg-custom-blue flex-col items-center flex-grow text-center px-4 gap-8">
+      <main className="w-full flex bg-custom-blue flex-col items-center flex-grow text-center px-4 gap-7">
         <h1 className="text-xl mt-6 mb-6 w-full text-left">
-          Bem-vindo, Francisco
+          Bem-vindo, Francisco!
         </h1>
         <VoiceButton />
         <TextInputButton />
