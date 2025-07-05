97c2f5442490b219bc276ce55cf7bcd95f0aa31d
GabrielAlves263 <jgabrielalves@alu.ufc.br>
2025-07-05 20:33:08 -0300

    feat: add Header component with logo and styling

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <div className="bg-blue-100 w-full p-4 flex items-center gap-2">
+      <img src="logoVivai.png" alt="Logo Viva Bem" className="h-6" />
+    </div>
+  );
+}
