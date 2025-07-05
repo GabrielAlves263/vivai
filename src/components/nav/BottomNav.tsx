b368796e98c1a86a08d8b6c4ed11234a66556df0
GabrielAlves263 <jgabrielalves@alu.ufc.br>
2025-07-05 20:46:40 -0300

    feat: add max-width to BottomNav component for improved layout

--- src/components/nav/BottomNav.tsx
+++ src/components/nav/BottomNav.tsx
@@ -1,6 +1,6 @@
 export default function BottomNav() {
   return (
-    <div className="fixed bottom-0 w-full flex justify-around p-4">
+    <div className="fixed bottom-0 w-full max-w-lg flex justify-around p-4">
       <img src="userIcon.svg" className="h-14 opacity-50" />
       <img src="homeIcon.svg" className="h-14 text-blue-500" />
       <img src="settingsIcon.svg" className="h-14 opacity-50" />
