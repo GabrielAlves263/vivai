cbc05ab82539e4652f661276c8a033090fb7dc06
GabrielAlves263 <jgabrielalves@alu.ufc.br>
2025-07-05 20:32:55 -0300

    feat: add TextInputButton component for user input

---
+++ src/components/buttons/TextInputButton.tsx
@@ -0,0 +1,7 @@
+export default function TextInputButton() {
+  return (
+    <button className="mt-6 px-6 py-3 rounded-lg border flex items-center gap-2 text-lg">
+      Digitar <img src="typeIcon.svg" alt="Teclado" className="h-5" />
+    </button>
+  );
+}
