d2ba181e92aa2b1f7bdb92a37006e5758edf8a41
GabrielAlves263 <jgabrielalves@alu.ufc.br>
2025-07-05 20:46:57 -0300

    feat: increase text size in VoiceButton component and enhance button styling

--- src/components/buttons/VoiceButton.tsx
+++ src/components/buttons/VoiceButton.tsx
@@ -3,11 +3,11 @@ import { FaMicrophone } from "react-icons/fa";
 export default function VoiceButton() {
   return (
     <div className="flex flex-col items-center gap-4">
-      <p className="text-lg">
+      <p className="text-xl">
         Fale com a <span className="text-red-400">Alice</span>
       </p>
-      <div className="rounded-full bg-blue-100 p-5 shadow-lg">
-        <button className="group transition-all rounded-full bg-blue-500 p-14 shadow-lg hover:p-16">
+      <div className="group rounded-full bg-blue-100 p-5 shadow-lg cursor-pointer">
+        <button className="transition-all rounded-full bg-blue-500 p-14 shadow-lg group-hover:p-16 cursor-pointer">
           <FaMicrophone size={80} className="group-hover:text-white" />
         </button>
       </div>
