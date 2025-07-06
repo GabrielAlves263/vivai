b5e2f7981ebc0f903fb359bf9d540d2e707a8faa
GabrielAlves263 <jgabrielalves@alu.ufc.br>
2025-07-05 21:24:43 -0300

    feat: enhance TextInputButton with icon and hover effects for improved user interaction

--- src/components/buttons/TextInputButton.tsx
+++ src/components/buttons/TextInputButton.tsx
@@ -1,7 +1,10 @@
+import { TbKeyboardFilled } from "react-icons/tb";
+
 export default function TextInputButton() {
   return (
-    <button className="mt-6 px-6 py-3 rounded-lg border flex items-center gap-2 text-lg">
-      Digitar <img src="typeIcon.svg" alt="Teclado" className="h-5" />
+    <button className="group mt-6 px-6 py-3 rounded-lg border flex items-center gap-2 text-lg cursor-pointer hover:text-blue-400 hover:px-7 transition-all">
+      Digitar{" "}
+      <TbKeyboardFilled className="size-6 group-hover:text-blue-400 transition-all" />
     </button>
   );
 }
