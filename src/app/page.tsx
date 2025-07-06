4b9930bded71d79033d58583187edf96e060e211
GabrielAlves263 <jgabrielalves@alu.ufc.br>
2025-07-06 09:22:57 -0300

    fix: adjust header margin for improved layout consistency

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
     <div className="min-h-screen flex flex-col w-md items-center justify-between bg-[#FAF9F6] border-x">
       <Header />
       <main className="w-full flex bg-custom-blue flex-col items-center flex-grow text-center px-10 gap-4">
-        <h1 className="text-xl mt-6 w-full text-left">
+        <h1 className="text-xl mt-4 w-full text-left">
           <strong>Bem-vindo,</strong> <br />
           Francisco!
         </h1>
