0372ea9ebb6456e94fa9318044ea648fe46b1204
GabrielAlves263 <jgabrielalves@alu.ufc.br>
2025-07-06 08:35:19 -0300

    fix: update VoiceButton text and button padding for improved clarity and styling

--- src/components/buttons/VoiceButton.tsx
+++ src/components/buttons/VoiceButton.tsx
@@ -30,15 +30,16 @@ export default function VoiceButton() {
 
   return (
     <div className="flex flex-col items-center gap-4">
-      <p className="text-xl">
-        Fale com a <span className="text-red-400">Alice</span>
+      <p className="text-2xl">
+        <strong className="font-black">APERTE O BOTÃO</strong> <br />
+        Para falar com a <span className="text-red-400">Alice</span>
       </p>
       <button
         onClick={handleClick}
         disabled={loading}
         className="group rounded-full bg-blue-100 p-5 shadow-lg cursor-pointer"
       >
-        <div className="transition-all rounded-full bg-blue-500 p-14 shadow-lg group-hover:p-16 cursor-pointer">
+        <div className="transition-all rounded-full bg-blue-500 p-10 shadow-lg group-hover:p-12 cursor-pointer">
           {loading ? (
             <span className="animate-pulse text-white text-lg size-20">
               ...
