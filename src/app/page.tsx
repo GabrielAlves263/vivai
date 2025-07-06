f7b438592ad42af763fc0e0e47451ba6f736ce4b
GabrielAlves263 <jgabrielalves@alu.ufc.br>
2025-07-06 07:48:06 -0300

    fix: adjust padding and improve welcome message formatting in Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,9 +7,10 @@ export default function Home() {
   return (
     <div className="min-h-screen flex flex-col w-md items-center justify-between bg-[#FAF9F6] border-x">
       <Header />
-      <main className="w-full flex bg-custom-blue flex-col items-center flex-grow text-center px-4 gap-7">
+      <main className="w-full flex bg-custom-blue flex-col items-center flex-grow text-center px-8 gap-7">
         <h1 className="text-xl mt-6 mb-6 w-full text-left">
-          Bem-vindo, Francisco!
+          <strong>Bem-vindo,</strong> <br />
+          Francisco!
         </h1>
         <VoiceButton />
         <TextInputButton />
