cbe913d5808b495d4f3af736b841eb90bf34de9f
GabrielAlves263 <jgabrielalves@alu.ufc.br>
2025-07-06 07:31:28 -0300

    feat: implement voice interaction functionality in VoiceButton component

--- src/components/buttons/VoiceButton.tsx
+++ src/components/buttons/VoiceButton.tsx
@@ -1,16 +1,53 @@
+"use client";
+
+import { useState } from "react";
+import { useAI } from "../../hooks/useAI";
+import { useSpeechRecognition } from "../../hooks/useSpeechRecognition";
+
 import { FaMicrophone } from "react-icons/fa";
 
 export default function VoiceButton() {
+  const { reconhecer } = useSpeechRecognition();
+  const { conversar, falar } = useAI();
+  const [loading, setLoading] = useState(false);
+
+  const handleClick = async () => {
+    try {
+      setLoading(true);
+      const pergunta = await reconhecer();
+      const resposta = await conversar(pergunta);
+      console.log("IA respondeu:", resposta);
+      await falar(resposta);
+    } catch (error) {
+      console.error(error);
+      alert(
+        "Algo deu errado! Certifique-se que o seu navegador é compatível com reconhecimento de voz (Chrome, Edge, etc)."
+      );
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
     <div className="flex flex-col items-center gap-4">
       <p className="text-xl">
         Fale com a <span className="text-red-400">Alice</span>
       </p>
-      <div className="group rounded-full bg-blue-100 p-5 shadow-lg cursor-pointer">
-        <button className="transition-all rounded-full bg-blue-500 p-14 shadow-lg group-hover:p-16 cursor-pointer">
-          <FaMicrophone size={80} className="group-hover:text-white" />
-        </button>
-      </div>
+      <button
+        onClick={handleClick}
+        disabled={loading}
+        className="group rounded-full bg-blue-100 p-5 shadow-lg cursor-pointer"
+      >
+        <div className="transition-all rounded-full bg-blue-500 p-14 shadow-lg group-hover:p-16 cursor-pointer">
+          {loading ? (
+            <span className="animate-pulse text-white text-lg size-20">
+              ...
+            </span>
+          ) : (
+            <FaMicrophone size={80} className="group-hover:text-white" />
+          )}
+        </div>
+      </button>
     </div>
   );
 }
