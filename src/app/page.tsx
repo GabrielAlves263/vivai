350a61b9de5018646f14311b378347c03d304285
GabrielAlves263 <jgabrielalves@alu.ufc.br>
2025-07-06 08:35:11 -0300

    feat: enhance TextInputButton with input field and icon integration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,8 +7,8 @@ export default function Home() {
   return (
     <div className="min-h-screen flex flex-col w-md items-center justify-between bg-[#FAF9F6] border-x">
       <Header />
-      <main className="w-full flex bg-custom-blue flex-col items-center flex-grow text-center px-8 gap-7">
-        <h1 className="text-xl mt-6 mb-6 w-full text-left">
+      <main className="w-full flex bg-custom-blue flex-col items-center flex-grow text-center px-10 gap-4">
+        <h1 className="text-xl mt-6 w-full text-left">
           <strong>Bem-vindo,</strong> <br />
           Francisco!
         </h1>
--- src/components/buttons/TextInputButton.tsx
+++ src/components/buttons/TextInputButton.tsx
@@ -1,10 +1,44 @@
-import { TbKeyboardFilled } from "react-icons/tb";
+"use client";
+import { useState } from "react";
+import { LuSendHorizontal } from "react-icons/lu";
 
 export default function TextInputButton() {
+  const [value, setValue] = useState("");
+  const [isFocused, setIsFocused] = useState(false);
+
+  // O input está "ativo" se estiver focado ou se tiver algum valor
+  const isActive = isFocused || value !== "";
+
   return (
-    <button className="group mt-6 px-6 py-3 rounded-lg border flex items-center gap-2 text-lg cursor-pointer hover:text-blue-400 hover:px-7 transition-all">
-      Digitar{" "}
-      <TbKeyboardFilled className="size-6 group-hover:text-blue-400 transition-all" />
-    </button>
+    <div className="relative w-full max-w-sm mt-6 bg-primary-light rounded-2xl">
+      <div>
+        <span
+          className={`
+          absolute left-4 top-1/2 -translate-y-1/2 flex items-center gap-2 
+          text-text pointer-events-none 
+          transition-all duration-200 ease-in-out
+          font-semibol
+          ${isActive ? "hidden" : ""}
+        `}
+        >
+          OU <b className="font-black">DIGITE</b> AQUI
+        </span>
+        <div className="absolute inset-y-0 right-0 flex items-center pr-4 pointer-events-none">
+          <LuSendHorizontal className="size-8 text-red" />
+        </div>
+      </div>
+      <input
+        type="text"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+        onFocus={() => setIsFocused(true)}
+        onBlur={() => setIsFocused(false)}
+        className="
+          w-full rounded-2xl px-4 py-3 
+          text-xl text-text focus:ring-2 focus:ring-blue-200 
+          focus:outline-none
+        "
+      />
+    </div>
   );
 }
