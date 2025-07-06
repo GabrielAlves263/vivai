b0a82777ac214b5d0bfd54b80debd05b2446bf0a
GabrielAlves263 <jgabrielalves@alu.ufc.br>
2025-07-06 09:22:45 -0300

    feat: integrate AI functionality in TextInputButton for enhanced user interaction

--- src/components/buttons/TextInputButton.tsx
+++ src/components/buttons/TextInputButton.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { useAI } from "@/hooks/useAI";
 import { useState } from "react";
 import { LuSendHorizontal } from "react-icons/lu";
 
@@ -6,8 +7,35 @@ export default function TextInputButton() {
   const [value, setValue] = useState("");
   const [isFocused, setIsFocused] = useState(false);
 
-  // O input está "ativo" se estiver focado ou se tiver algum valor
+  const { conversar, falar } = useAI();
+  const [loading, setLoading] = useState(false);
+
   const isActive = isFocused || value !== "";
+
+  const handleSubmit = async () => {
+    try {
+      if (value.trim() === "") return;
+      setLoading(true);
+      const resposta = await conversar(value);
+      console.log("IA respondeu:", resposta);
+      await falar(resposta);
+      setValue("");
+    } catch (error) {
+      console.error(error);
+      alert(
+        "Algo deu errado! Certifique-se que o seu navegador é compatível com reconhecimento de voz (Chrome, Edge, etc)."
+      );
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const handleKeyDown = (event: any) => {
+    if (event.key === "Enter") {
+      event.preventDefault();
+      handleSubmit();
+    }
+  };
 
   return (
     <div className="relative w-full max-w-sm mt-4 bg-primary-light rounded-2xl">
@@ -17,15 +45,18 @@ export default function TextInputButton() {
           absolute left-4 top-1/2 -translate-y-1/2 flex items-center gap-2 
           text-text pointer-events-none 
           transition-all duration-200 ease-in-out
-          font-semibol
+          font-semibold
           ${isActive ? "hidden" : ""}
         `}
         >
           OU <b className="font-black">DIGITE</b> AQUI
         </span>
-        <div className="absolute inset-y-0 right-0 flex items-center pr-4 pointer-events-none">
-          <LuSendHorizontal className="size-8 text-red" />
-        </div>
+        <button
+          onClick={handleSubmit}
+          className="absolute inset-y-0 right-0 flex items-center pr-4"
+        >
+          <LuSendHorizontal className="size-8 text-red cursor-pointer" />
+        </button>
       </div>
       <input
         type="text"
@@ -33,10 +64,11 @@ export default function TextInputButton() {
         onChange={(e) => setValue(e.target.value)}
         onFocus={() => setIsFocused(true)}
         onBlur={() => setIsFocused(false)}
+        onKeyDown={handleKeyDown}
         className="
           w-full rounded-2xl px-4 py-3 
           text-xl text-text focus:ring-2 focus:ring-blue-200 
-          focus:outline-none
+          focus:outline-none pr-14
         "
       />
     </div>
